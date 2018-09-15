8f50dc72b228e99bbfc60231b69de62065ed0bdb
erickkur65 <erickkurniawan65@gmail.com>
2018-09-15 11:00:04 +0700

    Fix jwt error

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,5 @@
+import time
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -11,3 +13,22 @@ class User(AbstractUser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.original_email = self.email
+
+    def full_name(self):
+        if not self.first_name:
+            return self.username
+
+        full_name = self.first_name
+        if self.last_name:
+            full_name += ' ' + self.last_name
+
+        return full_name
+
+    def save(self, *args, **kwargs):
+        # Build username
+        if not self.username:
+            unix_time = int(time.time())
+            full_name = self.full_name().replace(' ', '_')
+            self.username = f'{full_name}_{unix_time}'
+
+        super().save(*args, **kwargs)
